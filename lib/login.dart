9c171112d2aebc9c2e2e7e1496baff4b3efc13e0
faika benzaara <faikabenzaara@gmail.com>
2024-10-31 12:55:21 +0100

    version3

---
+++ lib/login.dart
@@ -0,0 +1,162 @@
+import 'package:flutter/material.dart';
+import 'signup.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'profile.dart';
+
+class LoginPage extends StatelessWidget {
+  final TextEditingController emailController = TextEditingController();
+  final TextEditingController passwordController = TextEditingController();
+
+  CollectionReference users = FirebaseFirestore.instance.collection('users');
+
+  Future<void> login(BuildContext context) async {
+    try {
+      // Here you can add your authentication logic.
+      // For demonstration, we’re just adding user data to Firestore.
+      await users.add({
+        'email': emailController.text,
+        'password': passwordController
+            .text, // Consider hashing the password in a real app
+      });
+      print("User logged in");
+      ScaffoldMessenger.of(context).showSnackBar(
+          const SnackBar(content: Text('User successfully logged in')));
+
+      // Navigate to the Profile page after successful login
+      Navigator.pushReplacement(
+        context,
+        MaterialPageRoute(builder: (context) => ProfilePage()),
+      );
+    } catch (error) {
+      print("Failed to log in user: $error");
+      ScaffoldMessenger.of(context).showSnackBar(
+          SnackBar(content: Text('Failed to log in user: $error')));
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.white,
+      body: Center(
+        child: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 32.0),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Container(
+                width: 80,
+                height: 80,
+                child: Image.asset(
+                  "assets/images/image1.png",
+                  height: 250,
+                ),
+              ),
+              SizedBox(height: 20),
+              Text(
+                'Welcome Back',
+                style: TextStyle(
+                  fontSize: 26,
+                  fontWeight: FontWeight.bold,
+                  color: Colors.blue[900],
+                ),
+              ),
+              Text(
+                'Sign in to continue',
+                style: TextStyle(
+                  fontSize: 16,
+                  color: Colors.grey[600],
+                ),
+              ),
+              SizedBox(height: 40),
+              TextField(
+                controller: emailController,
+                decoration: InputDecoration(
+                  filled: true,
+                  fillColor: Colors.grey[100],
+                  labelText: 'Email',
+                  labelStyle: TextStyle(
+                      color: const Color.fromARGB(255, 201, 202, 204)),
+                  prefixIcon: Icon(Icons.email,
+                      color: const Color.fromARGB(255, 51, 198, 243)),
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(12),
+                    borderSide: BorderSide.none,
+                  ),
+                ),
+              ),
+              SizedBox(height: 20),
+              TextField(
+                controller: passwordController,
+                obscureText: true,
+                decoration: InputDecoration(
+                  filled: true,
+                  fillColor: Colors.grey[100],
+                  labelText: 'Password',
+                  labelStyle: TextStyle(
+                      color: const Color.fromARGB(255, 198, 199, 201)),
+                  prefixIcon: Icon(Icons.lock,
+                      color: const Color.fromARGB(255, 44, 186, 241)),
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(12),
+                    borderSide: BorderSide.none,
+                  ),
+                ),
+              ),
+              SizedBox(height: 10),
+              Align(
+                alignment: Alignment.centerRight,
+                child: TextButton(
+                  onPressed: () {},
+                  child: Text(
+                    'Forgot Password?',
+                    style: TextStyle(color: Colors.blue[900]),
+                  ),
+                ),
+              ),
+              SizedBox(height: 20),
+              SizedBox(
+                width: double.infinity,
+                child: ElevatedButton(
+                  style: ElevatedButton.styleFrom(
+                    backgroundColor: Colors.blue[900],
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(12),
+                    ),
+                    padding: EdgeInsets.symmetric(vertical: 16),
+                  ),
+                  onPressed: () {
+                    login(context); // Call login function here
+                  },
+                  child: Text(
+                    'LOGIN',
+                    style: TextStyle(
+                      fontSize: 18,
+                      color: Colors.white,
+                    ),
+                  ),
+                ),
+              ),
+              SizedBox(height: 20),
+              GestureDetector(
+                onTap: () {
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(builder: (context) => SignupPage()),
+                  );
+                },
+                child: Text(
+                  "Don't have an account? Create a new account",
+                  style: TextStyle(
+                    color: const Color.fromARGB(255, 10, 10, 10),
+                    fontWeight: FontWeight.w500,
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
---
+++ lib/profile.dart
@@ -0,0 +1,108 @@
+import 'package:flutter/material.dart';
+import 'personaldata.dart';
+
+class ProfilePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.white,
+      body: SafeArea(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Padding(
+              padding: const EdgeInsets.all(16.0),
+              child: Row(
+                children: [
+                  CircleAvatar(
+                    radius: 40,
+                    backgroundImage: AssetImage('assets/images/image2.jpg'),
+                  ),
+                  SizedBox(width: 16),
+                  Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text(
+                        'William John Malik',
+                        style: TextStyle(
+                          fontSize: 20,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                      Text(
+                        'Aggressive Investor',
+                        style: TextStyle(
+                          fontSize: 16,
+                          color: Colors.grey,
+                        ),
+                      ),
+                    ],
+                  ),
+                ],
+              ),
+            ),
+            Divider(),
+            ListTile(
+              leading: Icon(Icons.person),
+              title: Text('Personal Data'),
+              onTap: () {
+                Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                        builder: (context) => PersonalDataPage()));
+              },
+            ),
+            ListTile(
+              leading: Icon(Icons.settings),
+              title: Text('Settings'),
+              onTap: () {},
+            ),
+            ListTile(
+              leading: Icon(Icons.document_scanner),
+              title: Text('E-Statement'),
+              onTap: () {},
+            ),
+            ListTile(
+              leading: Icon(Icons.code),
+              title: Text('Referral Code'),
+              onTap: () {},
+            ),
+            ListTile(
+              leading: Icon(Icons.help),
+              title: Text('FAQs'),
+              onTap: () {},
+            ),
+            ListTile(
+              leading: Icon(Icons.book),
+              title: Text('Our Handbook'),
+              onTap: () {},
+            ),
+            ListTile(
+              leading: Icon(Icons.group),
+              title: Text('Community'),
+              onTap: () {
+                //Navigator.push(context,
+                // MaterialPageRoute(builder: (context) => CommunityPage()));
+              },
+            ),
+            Spacer(),
+            Padding(
+              padding: const EdgeInsets.all(16.0),
+              child: ElevatedButton(
+                style: ElevatedButton.styleFrom(
+                  minimumSize: Size(double.infinity, 50),
+                  backgroundColor: Colors.blue[100],
+                ),
+                onPressed: () {},
+                child: Text(
+                  "Feel Free to Ask, We’re Ready to Help",
+                  style: TextStyle(color: Colors.blue[900]),
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
